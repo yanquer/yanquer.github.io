4b2c1f7df1e749d0e845d6d750ba79871208c623
烟雀 <yanquer@qq.com>
2025-02-28 15:26:11 +0800

    add something page

--- theme/js/config.js
+++ theme/js/config.js
@@ -22,11 +22,49 @@ window._skel_panels_config = {
 };
 
 jQuery(function() {
-	$('#nav > ul').dropotron({ 
+	$('#nav > ul').dropotron({
 		offsetY: -17,
 		offsetX: -1,
 		mode: 'fade',
 		noOpenerFade: true,
 		detach: false
 	});
+
 });
+
+
+// ========================================
+// 一些自定义操作
+// ========================================
+
+// 代码块支持复制 , 最大/小化
+function codeBlockSupport() {
+	// 复制代码块
+	var copyCode = function() {
+		var code = $(this).parent().next().text();
+		var $temp = $("<textarea>");
+		$("body").append($temp);
+		$temp.val(code).select();
+		document.execCommand("copy");
+		$temp.remove();
+		alert('代码已复制到剪贴板');
+	};
+	$(".code-block .copy").click(copyCode);
+
+	// 最大化代码块
+	var maxCode = function() {
+		var $codeBlock = $(this).parent().next();
+		if ($codeBlock.hasClass("max")) {
+			$codeBlock.removeClass("max");
+		} else {
+			$codeBlock.addClass("max");
+		}
+	};
+	$(".code-block .max").click(maxCode);
+}
+
+
+
+
+
+
